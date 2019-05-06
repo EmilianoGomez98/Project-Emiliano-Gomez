a0cd0128892615b05be1b884eccc85b1fe7efdcf
EmilianoGomez98 <emiliano.p.gomez98@gmail.com>
2019-05-06 19:44:25 -0300

    Creo tests para los controlladores dedicados a comandos 'append' y 'preppend'
    Creo tests que prueban que se hagan renders o redirects correctamente
    bajo condiciones especificas.

--- spec/controllers/appends_controller_spec.rb
+++ spec/controllers/appends_controller_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe AppendsController, type: :controller do
+  describe "POST #create" do
+    before(:each) do
+      data = Memdata.new("0","1","2","12")
+      Memdata.set_key("Ab23",data)
+    end
 
+    context "Successful append; Renders storage_success" do
+
+      it "Successful append(1)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "2",:value => "12"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+      it "Successful append(2)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "1",:value => "1"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+      it "Successful append(3)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "3",:value => "123"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+    end
+
+    context "Not successful, bad input, redirects to /append" do
+
+      it "Bad bytes input" do
+        post 'create', :params => {:key => "Ab23",:bytes => "a",:value => "1"}
+        expect(response).to redirect_to("/append")
+      end
+
+      it "value.length>bytes" do
+        post 'create', :params => {:key => "Ab23",:bytes => "1",:value => "123"}
+        expect(response).to redirect_to("/append")
+      end
+    end
+
+    context "Not successful, empty necesary field, redirects to /append" do
+      it "key = nil" do
+        post 'create', :params => {:key => "",:bytes => "1",:value => "1"}
+        expect(response).to redirect_to("/append")
+      end
+
+      it "bytes = nil" do
+        post 'create', :params => {:key => "Ab23",:bytes => "",:value => "1"}
+        expect(response).to redirect_to("/append")
+      end
+    end
+
+    context "Not successful, append conditions not met, redirects to /append" do
+      it "Key didn't already exist" do
+        post 'create', :params => {:key => "1",:bytes => "1",:value => "1"}
+        expect(response).to redirect_to("/append")
+      end
+
+      it "Key already expired after one second" do
+        sleep(1)
+        post 'create', :params => {:key => "Ab23",:bytes => "2",:value => "12"}
+        expect(response).to redirect_to("/append")
+      end
+    end
+  end
 end
--- spec/controllers/preppends_controller_spec.rb
+++ spec/controllers/preppends_controller_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe PreppendsController, type: :controller do
+  describe "POST #create" do
+    before(:each) do
+      data = Memdata.new("0","1","2","12")
+      Memdata.set_key("Ab23",data)
+    end
 
+    context "Successful preppend; Renders storage_success" do
+
+      it "Successful preppend(1)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "2",:value => "12"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+      it "Successful preppend(2)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "1",:value => "1"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+      it "Successful preppend(3)" do
+        post 'create', :params => {:key => "Ab23",:bytes => "3",:value => "123"}
+        expect(response).to render_template("pages/storage_success", "layouts/application")
+      end
+
+    end
+
+    context "Not successful, bad input, redirects to /preppend" do
+
+      it "Bad bytes input" do
+        post 'create', :params => {:key => "Ab23",:bytes => "a",:value => "1"}
+        expect(response).to redirect_to("/preppend")
+      end
+
+      it "value.length>bytes" do
+        post 'create', :params => {:key => "Ab23",:bytes => "1",:value => "123"}
+        expect(response).to redirect_to("/preppend")
+      end
+    end
+
+    context "Not successful, empty necesary field, redirects to /preppend" do
+      it "key = nil" do
+        post 'create', :params => {:key => "",:bytes => "1",:value => "1"}
+        expect(response).to redirect_to("/preppend")
+      end
+
+      it "bytes = nil" do
+        post 'create', :params => {:key => "Ab23",:bytes => "",:value => "1"}
+        expect(response).to redirect_to("/preppend")
+      end
+    end
+
+    context "Not successful, preppend conditions not met, redirects to /preppend" do
+      it "Key didn't already exist" do
+        post 'create', :params => {:key => "1",:bytes => "1",:value => "1"}
+        expect(response).to redirect_to("/preppend")
+      end
+
+      it "Key already expired after one second" do
+        sleep(1)
+        post 'create', :params => {:key => "Ab23",:bytes => "2",:value => "12"}
+        expect(response).to redirect_to("/preppend")
+      end
+    end
+
+  end
 end
