7e2e31075566c1c4a4f1083b676e1fb67ad391bc
EmilianoGomez98 <emiliano.p.gomez98@gmail.com>
2019-05-06 20:16:32 -0300

    Creo tests basicos para los comandos 'get' y 'getcas'

--- spec/controllers/getcas_controller_spec.rb
+++ spec/controllers/getcas_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe GetcasController, type: :controller do
+  describe "POST #show" do
+    before(:each) do
+      data = Memdata.new("0","1","2","12")
+      Memdata.set_key("Ab23",data)
+    end
 
+    context "Successfully retrieves data for the given keys" do
+      it "Retrieves data for the key selected" do
+        post 'show', :params => {:keys => "Ab23"}
+        expect(response).to render_template("getcas/show", "layouts/application")
+      end
+    end
+
+    context "Not successful, bad input" do
+      it "Key field is empty" do
+        post 'show', :params => {:keys => ""}
+        expect(response).to redirect_to("/getcas")
+      end
+    end
+
+  end
 end
