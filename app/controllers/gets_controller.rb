8ba0e8a549b2bdb4a1121f151518dd8032117f73
EmilianoGomez98 <emiliano.p.gomez98@gmail.com>
2019-05-06 20:14:24 -0300

    Agregados mensajes de error a los comandos 'get' y 'getcas'
    Agrego un condicional en los controladores getcas y gets para validar
    si el campo esta vacio al momento de buscar, en caso de que lo este, se
    redirige al campo nuevamente, con un mensaje 'ERROR'.

--- app/controllers/gets_controller.rb
+++ app/controllers/gets_controller.rb
@@ -5,15 +5,19 @@ class GetsController < ApplicationController
 
   def show
     keys= params[:keys].split(/\W+/)
-    @dataHash = Hash.new
-    keys.each do |key|
-      if Memdata.has_key?(key)
-        if !Memdata.is_expired?(key)
-          @dataHash[key]=(Memdata.get_data(key))
-        else
-          Memdata.delete_expired(key)
+    if !keys.blank?
+      @dataHash = Hash.new
+      keys.each do |key|
+        if Memdata.has_key?(key)
+          if !Memdata.is_expired?(key)
+            @dataHash[key]=(Memdata.get_data(key))
+          else
+            Memdata.delete_expired(key)
+          end
         end
       end
+    else
+      redirect_to get_path, :flash => { :error => "ERROR" }
     end
   end
 
